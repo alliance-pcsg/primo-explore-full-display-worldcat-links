bd531a1ae5d68a3bcc9e942b98d38b91f3d9db45
Jeremy McWilliams <jeremym@lclark.edu>
2018-09-12 15:30:42 -0700

    cleaned up some stuff

--- js/fulldisplay-worldcat.module.js
+++ js/fulldisplay-worldcat.module.js
@@ -3,10 +3,6 @@ angular.module('fulldisplay-worldcat', []).component('prmFullViewServiceContaine
   controller: function controller($scope, $element) {
     this.$onInit = function () {
 
-
-
-
-      $scope.wcStyle={"display" : "none"}
       //$scope.wcDisplay = true; /* default hides template */
       //$scope.imagePath=oadoiOptions.imagePath;
       //var email=oadoiOptions.email;
@@ -16,38 +12,19 @@ angular.module('fulldisplay-worldcat', []).component('prmFullViewServiceContaine
 
       if (section == "getit_link1_0") {
 
-
-
-
-
-
-
         console.log($element);
         $scope.wcDisplay = true;
         console.log($scope);
         console.log($scope.$parent.$parent.$parent.$parent.$parent.$parent.$ctrl.item.delivery);
 
-
-//var el = angular.element( $element);
-
         $scope.$watch('$parent.$parent.$parent.$parent.$parent.$parent.$ctrl.item.delivery.availability[0]', function (newValue, oldValue, scope) {
           var avail=$scope.$parent.$parent.$parent.$parent.$parent.$parent.$ctrl.item.delivery.availability[0];
           console.log(avail);
             if (newValue=="does_not_exist_in_my_institution_unavailable"){
-              //$scope.wcDisplay = true;
-              //angular.copy(true, $scope.wcDisplay);
-              //$scope.oaClass="ng-show";
-              //$scope.displayClass="wcfdShow";
-              //$scope.wcStyle={"display" : "block"};
-            //  $element.children().removeClass("ng-hide");
-              //el.children().css("display", "block");
               var verdict= true;
-              //unregister();
             }
 
             $scope.showIt=function(){
-
-
               return verdict;
             }
 
