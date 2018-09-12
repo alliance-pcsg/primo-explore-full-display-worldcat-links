6d0346203b1dba51ede659999556837fce514e21
Jeremy McWilliams <jeremym@lclark.edu>
2018-09-12 14:41:44 -0700

    $watch interaction working

--- js/fulldisplay-worldcat.module.js
+++ js/fulldisplay-worldcat.module.js
@@ -4,6 +4,9 @@ angular.module('fulldisplay-worldcat', []).component('prmFullViewServiceContaine
     this.$onInit = function () {
 
 
+
+
+      $scope.wcStyle={"display" : "none"}
       //$scope.wcDisplay = true; /* default hides template */
       //$scope.imagePath=oadoiOptions.imagePath;
       //var email=oadoiOptions.email;
@@ -12,26 +15,42 @@ angular.module('fulldisplay-worldcat', []).component('prmFullViewServiceContaine
 
 
       if (section == "getit_link1_0") {
+
+
+
+
+
+
+
+        console.log($element);
         $scope.wcDisplay = true;
         console.log($scope);
         console.log($scope.$parent.$parent.$parent.$parent.$parent.$parent.$ctrl.item.delivery);
-        var avail=$scope.$parent.$parent.$parent.$parent.$parent.$parent.$ctrl.item.delivery.availability[0];
+
+
+//var el = angular.element( $element);
 
         $scope.$watch('$parent.$parent.$parent.$parent.$parent.$parent.$ctrl.item.delivery.availability[0]', function (newValue, oldValue, scope) {
-          console.log(newValue);
-
+          var avail=$scope.$parent.$parent.$parent.$parent.$parent.$parent.$ctrl.item.delivery.availability[0];
+          console.log(avail);
             if (newValue=="does_not_exist_in_my_institution_unavailable"){
-              $scope.wcDisplay = true;
-              angular.copy(true, $scope.wcDisplay);
-              $scope.oaClass="ng-show";
-              unregister();
-
-
-
+              //$scope.wcDisplay = true;
+              //angular.copy(true, $scope.wcDisplay);
+              //$scope.oaClass="ng-show";
+              //$scope.displayClass="wcfdShow";
+              //$scope.wcStyle={"display" : "block"};
+            //  $element.children().removeClass("ng-hide");
+              //el.children().css("display", "block");
+              var verdict= true;
+              //unregister();
             }
-            else{$scope.wcDisplay = false;}
 
-            //...
+            $scope.showIt=function(){
+
+
+              return verdict;
+            }
+
         }, true);
 
 
@@ -45,5 +64,5 @@ angular.module('fulldisplay-worldcat', []).component('prmFullViewServiceContaine
       }
     };
   },
-  template: '<div ng-show="{{wcDisplay}}"><h3 class="medium-uppercase-bold">Check WorldCat</h3><p style="font-size:75%;">If Summit borrowing is unavailaible for this title, follow a link below to request via Interlibrary Loan:</p></div>'
+  template: '<div ng-show="showIt()"><h3 class="medium-uppercase-bold">Check WorldCat</h3><p style="font-size:75%;">If Summit borrowing is unavailaible for this title, follow a link below to request via Interlibrary Loan:</p></div>'
 });
